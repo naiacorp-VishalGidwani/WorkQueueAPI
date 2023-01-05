d9a21c74ea91ad088ab4fba45d809cfb1012bda6
Vishal.Gidwani <Vishal.Gidwani@naiacorp.net>
2023-01-05 12:08:06 +0530

    added db connnection details with secrets file

--- Model/WorkQueueResponse.cs
+++ Model/WorkQueueResponse.cs
@@ -3,5 +3,7 @@
     public class WorkQueueResponse
     {
         public int Id { get; set; }
+        public String? ConnectionString { get; set; }
+        public String? Database { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
+using MongoDB.Driver;
 using WorkQueueAPI.Model;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("secrets.json");
+
+String connectionString = builder.Configuration["ConnectionString"];
+String databaseName = builder.Configuration["DatabaseName"];
+
 var app = builder.Build();
+
+var client = new MongoClient(
+    connectionString
+);
+
+var db = client.GetDatabase(databaseName);
 
 app.MapGet("/", () => "Hello World!");
 
@@ -9,6 +22,8 @@ app.MapGet("/work-queue", () => {
     return new WorkQueueResponse()
     {
         Id = 1,
+        ConnectionString = connectionString,
+        Database = databaseName
     };
 });
 
